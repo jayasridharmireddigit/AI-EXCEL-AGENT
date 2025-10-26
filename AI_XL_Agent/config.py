f3fb37cfc89cd1bf3e6ce48ba390e25e9e70283d
Jayasri Dharmireddi <jayasridharmireddi@gmail.com>
2025-10-26 16:54:44 +0530

    Update config.py

--- AI_XL_Agent/config.py
+++ AI_XL_Agent/config.py
@@ -1,3 +1,4 @@
-GEMINI_API_KEY = "AIzaSyBjgCM3-hVCTkO4DFs5bMdpABCcHJ1ANXg"
+GEMINI_API_KEY = "your key"
 excel_path = r"C:\Users\ADMIN\Desktop\AI_XL_Agent\data\valuation_test_case.xlsx"
+
 db_path = r"C:\Users\ADMIN\Desktop\AI_XL_Agent\workbook.db"
